57f2adb23e5d8cc19181fff1b426567acb49c02c
jdavis218 <jennadavis92@gmail.com>
2016-08-01 17:31:48 -0700

    resolve linting errors

--- src/reactParser.js
+++ src/reactParser.js
@@ -31,18 +31,26 @@ function getReactStates(node) {
  * @returns {Array} Array of all JSX props on React component
  */
 function getReactProps(node, parent) {
-  if (node.openingElement.attributes.length === 0 || htmlElements.indexOf(node.openingElement.name.name) > 0) return {};
+  if (node.openingElement.attributes.length === 0 ||
+  htmlElements.indexOf(node.openingElement.name.name) > 0) return {};
   const result = node.openingElement.attributes
     .map(attribute => {
       const name = attribute.name.name;
       let valueName;
       if (attribute.value === null) valueName = undefined;
       else if (attribute.value.type === 'Literal') valueName = attribute.value.value;
-      else if (attribute.value.expression.type === 'Literal') valueName = attribute.value.expression.value;
-      else if (attribute.value.expression.type === 'Identifier') valueName = attribute.value.expression.name;
-      else if (attribute.value.expression.type === 'CallExpression') valueName = attribute.value.expression.callee.object.property.name;
-      else if (attribute.value.expression.type === 'BinaryExpression') valueName = attribute.value.expression.left.name + attribute.value.expression.operator + (attribute.value.expression.right.name || attribute.value.expression.right.value);
-      else if (attribute.value.expression.type === 'MemberExpression') {
+      else if (attribute.value.expression.type === 'Literal') {
+        valueName = attribute.value.expression.value;
+      } else if (attribute.value.expression.type === 'Identifier') {
+        valueName = attribute.value.expression.name;
+      } else if (attribute.value.expression.type === 'CallExpression') {
+        valueName = attribute.value.expression.callee.object.property.name;
+      } else if (attribute.value.expression.type === 'BinaryExpression') {
+        valueName = attribute.value.expression.left.name
+          + attribute.value.expression.operator
+          + (attribute.value.expression.right.name
+          || attribute.value.expression.right.value);
+      } else if (attribute.value.expression.type === 'MemberExpression') {
         let current = attribute.value.expression;
         while (current && current.property) {
           //  && !current.property.name.match(/(state|props)/)
